bd64431f6af75dc6139969e38ddab9b4b5062ad3
Jonathan Raiman <jonathanraiman@gmail.com>
2014-11-05 18:17:21 -0500

    added tensor networks

--- cython_lstm/__init__.py
+++ cython_lstm/__init__.py
@@ -1,5 +1,6 @@
 import pyximport, numpy as np
 pyximport.install(setup_args={"include_dirs": np.get_include()})
-from .cython_utils import vector_outer_product
+from .cython_utils import vector_outer_product, tensor_delta_down, tensor_delta_down_with_output
 
-__all__ = ["vector_outer_product"]
+__all__ = ["vector_outer_product", "tensor_delta_down", "tensor_delta_down_with_output"]
+
--- cython_lstm/cython_utils.pyx
+++ cython_lstm/cython_utils.pyx
@@ -18,7 +18,18 @@ cdef sger_ptr sger=<sger_ptr>PyCapsule_GetPointer(fblas.sger._cpointer , NULL)  
 cdef void outer_prod(REAL_t*  x, REAL_t* y, REAL_t * out, int x_len, int y_len):
     sger(&y_len, &x_len, &ONEF, y, &ONE, x, &ONE, out, &y_len)
 
-def vector_outer_product(np.ndarray[REAL_t, ndim=2] _x, np.ndarray[REAL_t, ndim=2] _y):
+ctypedef void (*dgemv_ptr) (char *trans, int *m, int *n,\
+    float *alpha, float *a, int *lda, float *x, int *incx,\
+    float *beta,  float *y, int *incy)
+
+ctypedef float (*sdot_ptr) (const int *N, const float *X, const int *incX, const float *Y, const int *incY) nogil
+
+
+cdef sdot_ptr sdot=<sdot_ptr>PyCapsule_GetPointer(fblas.sdot._cpointer, NULL) # float = dot(x, y)
+cdef dgemv_ptr dgemv=<dgemv_ptr>PyCapsule_GetPointer(fblas.dgemv._cpointer, NULL)
+
+
+cpdef np.ndarray[REAL_t, ndim=3] vector_outer_product(np.ndarray[REAL_t, ndim=2] _x, np.ndarray[REAL_t, ndim=2] _y):
     
     cdef int i, length = _x.shape[0], x_len = _x.shape[1], y_len = _y.shape[1]
     cdef int box_size = x_len * y_len
@@ -31,8 +42,37 @@ def vector_outer_product(np.ndarray[REAL_t, ndim=2] _x, np.ndarray[REAL_t, ndim=
     cdef REAL_t[:,:] x_view = _x
     cdef REAL_t[:,:] y_view = _y
     
-    
     for i in range(length):
         outer_prod(&x_view[i,0], &y_view[i,0], &result[i,0,0], x_len, y_len)
     
     return result.transpose((1,2,0))
+
+def tensor_delta_down_with_output(
+    np.ndarray[REAL_t, ndim=3] tensor,
+    np.ndarray[REAL_t, ndim=2] dEdz,
+    np.ndarray[REAL_t, ndim=2] input,
+    np.ndarray[REAL_t, ndim=2] out):
+    
+    cdef:
+        int size = dEdz.shape[1]
+        np.ndarray[REAL_t, ndim=3] outer_dotted   = vector_outer_product(dEdz, input)
+    
+    for i in range(size):
+        out += np.dot(tensor[i,:,:], outer_dotted[i,:,:]).T
+        out += np.dot(tensor[i,:,:].T, outer_dotted[i,:,:]).T
+
+def tensor_delta_down(
+    np.ndarray[REAL_t, ndim=3] tensor,
+    np.ndarray[REAL_t, ndim=2] dEdz,
+    np.ndarray[REAL_t, ndim=2] input):
+    
+    cdef:
+        int size = dEdz.shape[1]
+        np.ndarray[REAL_t, ndim=2] delta_unbiased = np.zeros_like(input, dtype=REAL)
+        np.ndarray[REAL_t, ndim=3] outer_dotted   = vector_outer_product(dEdz, input)
+    
+    for i in range(size):
+        delta_unbiased += np.dot(tensor[i,:,:], outer_dotted[i,:,:]).T
+        delta_unbiased += np.dot(tensor[i,:,:].T, outer_dotted[i,:,:]).T
+    
+    return delta_unbiased
