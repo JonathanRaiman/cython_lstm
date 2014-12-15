ebe29224200808bc1d21c869221e9b3b2659ca66
Jonathan Raiman <jonathanraiman@gmail.com>
2014-12-14 21:31:29 -0500

    added AdaGrad, AdaDelta logic to trainer, more polyvalent class

--- cython_lstm/trainer.py
+++ cython_lstm/trainer.py
@@ -1,33 +1,96 @@
+import numpy as np
+
 class Trainer():
     """
-    Dead simple training algorithm using backpropagation
-    for a, artificial neural network.
+    Get the updates for a gradient descent optimizer using
+    SGD, AdaDelta, or AdaGrad.
+
+    Call train with a pair of input output to perform
+    gradient descent on a model.
     
+    Parameters are updated in place.
+
+    Inputs
+    ------
+
+    model    Network         : The model to optimizer.
+                               Must have the methods:
+                               * `get_parameters()`,
+                               * `get_gradients()`
+                               * `clear()`,
+                               * `activate(input)`, 
+                               * `backpropagate(output)`,
+                               * `error(output)`.
+    lr       float           : base learning rate for
+                               adagrad and SGD
+    eps      float           : numerical stability value
+                               to not divide by zero
+                               sometimes
+    rho      float           : adadelta hyperparameter.
+    method   str             : 'adagrad', 'adadelta', or 'sgd'.
+
     """
-    def __init__(self, network, alpha = 0.035):
+    def __init__(self, model,
+        eps=1e-6,
+        rho=0.95,
+        lr = 0.01,
+        max_norm=5.0,
+        method = "adadelta"):
         # should freeze the structure of the network or have
         # robust method of linking to the elements inside
-        self.network = network
-        self.parameters = network.get_parameters()
-        self.gradients = network.get_gradients()
-        self._alpha = alpha
-        
-    def train(self, input, output):
-        
-        # reset network activations
-        self.network.clear()
-        
-        # run data through network
-        self.network.activate(input)
-        
-        # backpropagate error through net:
-        self.network.backpropagate(output)
-        
-        # collect cost:
-        cost = self.network.error(output).sum()
-        
-        # update weights:
+        self.model      = model
+        self._method    = method
+        self.parameters = model.get_parameters()
+        self.gradients  = model.get_gradients()
+        self.lr         = lr
+        self.rho        = rho
+        self.eps        = eps
+
+        if method == "adadelta":
+            self.gsums   = [np.zeros_like(param) for param in self.parameters]
+            self.xsums   = [np.zeros_like(param) for param in self.parameters]
+            self._grad_update = self.adadelta_update
+        elif method == "adagrad":
+            self.gsums   = [np.zeros_like(param) for param in self.parameters]
+            self._grad_update = self.adagrad_update
+        else:
+            self._grad_update = self.sgd_update
+
+    @property
+    def method(self):
+        return self._method
+
+    def adadelta_update(self):
+        for gparam, param, gsum, xsum in zip(self.gradients, self.parameters, self.gsums, self.xsums):
+            gsum[:] = (self.rho * gsum + (1. - self.rho) * (gparam **2)).astype(param.dtype, False)
+            dparam = -np.sqrt((xsum + self.eps) / (gsum + self.eps)) * gparam
+            xsum[:] = (self.rho * xsum + (1. - self.rho) * (dparam **2)).astype(param.dtype, False)
+            param += dparam
+    def adagrad_update(self):
+        for gparam, param, gsum in zip(self.gradients, self.parameters, self.gsums):
+            gsum[:] =  (gsum + (gparam ** 2)).astype(param.dtype, False)
+            param -=  self.lr * (gparam / (np.sqrt(gsum + self.eps)))
+
+    def sgd_update(self):
         for gparam, param in zip(self.gradients, self.parameters):
             param -= (self._alpha * gparam)
+
+
+    def train(self, input, output):
+        
+        # reset model activations
+        self.model.clear()
+        
+        # run data through model
+        self.model.activate(input)
+        
+        # backpropagate error through net:
+        self.model.backpropagate(output)
+        
+        # collect cost:
+        cost = self.model.error(output).sum()
+        
+        # update weights:
+        self._grad_update()
             
         return cost
