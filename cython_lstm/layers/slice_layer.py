fixed backprop through time, probably made things exponentially slower too :(

cython_lstm/layers/slice_layer.py
@@ -1,44 +1,60 @@
 from .base_layer import BaseLayer
+import numpy as np
 
 class SliceLayer(BaseLayer):
     """
-
     Takes a subset of its input, and passes
     it forward.
-    In temporal sequences, this takes the last
-    element of a sequence and passes it onwards.
-
     """
 
     def __init__(self, index):
         BaseLayer.__init__(self)
-        self.index         = index
-        self._activation   = None
-        self.dimensionless = True
+        if not hasattr(index, '__len__'):
+            index = (index,)
+        self.index = index
+        self.gradients = []
 
-    def forward_propagate(self, x):
-        return [layer.forward_propagate(x[self.index]) for layer in self._temporal_forward_layers]
+    def select_slice(self, input):
+        if type(input) is list:
+            if len(self.index) > 1:
+                return input[self.index[0]][self.index[1:]]
+            else:
+                return input[self.index[0]]
+        else:
+            return input[self.index]
 
-    def clear(self):
-        self.step        = 0
-
-    def allocate_activation(self, *args):
-        pass
-
-    @property
-    def input_size(self):
+    def activate(self, x, out=None):
         """
-        The input size is the activation of the previous node.
+        Activation for a slice layer is dead simple:
 
-        This value is currently incorrect for 3d tensors.
-        """
-        return self._backward_layer.input_size if self._backward_layer is not None else None
+        > y = x[index]
 
-    @property
-    def output_size(self):
         """
-        The output size is the result of a slice on the activation of the previous node.
+        input = self.select_slice(x[0])
 
-        This value is currently incorrect for 3d tensors.
+        if out is None:
+            # no copy is performed here
+            # thankfully
+            self._activation = input
+            return self._activation
+        else:
+            out[:] = input
+            return out
+
+    def update_grad_input(self, input, output, grad_output):
         """
-        return self._backward_layer.input_size if self._backward_layer is not None else None
+        Gradient for a slice is simply zeros for all non
+        sliced dimensions, and pass the grad_output inside
+        the sliced piece.
+
+        > grad = np.zeros_like(input)
+        > grad[index] = ParentGradient
+
+        """
+        if self.gradinput is None:
+            if type(input) is list:
+                self.gradinput = [np.zeros_like(piece) for piece in input]
+            else:
+                self.gradinput = np.zeros_like(input)
+        self.select_slice(self.gradinput)[:] = grad_output
+        return self.gradinput

cython_lstm/network.py
@@ -1,12 +1,15 @@
 import numpy as np
+from .error import MSE
 
 class Network():
     """
     Create a simple neural network
     """
-    def __init__(self, dtype=np.float32):
+    def __init__(self, metric = MSE, dtype=np.float32):
         self.dtype = dtype
         self.layers = []
+        assert(hasattr(metric, 'error') and hasattr(metric, 'dEdy')), "Metric must implement error and error derivative."
+        self.error_metric = metric
         self._output_layer = None
         self._input_layer = None
         self._data_layer = DataLayer()
@@ -67,9 +70,7 @@ class Network():
         last_layer = self._output_layer
         # special error is used here to initialize the backpropagation
         # procedure
-        error_signal = last_layer.dEdy(last_layer._activation, target)
-        # now transition to the layer before this one:
-        last_layer = last_layer._backward_layer
+        error_signal = self.error_metric.dEdy(last_layer._activation, target)
         while last_layer is not None and last_layer is not self._data_layer:
             # we pass down the current error signal
             error_signal = last_layer.update_grad_input(
@@ -79,11 +80,20 @@ class Network():
             # step down by a layer.
             last_layer = last_layer._backward_layer
 
-
-
+    def set_error(self, metric):
+        """
+        Set the error metric that should
+        be used. The choices are:
+            * MSE
+            * BinaryCrossEntropy
+            * TanhBinayCrossEntropy
+            * CategoricalCrossEntropy
+        """
+        assert(hasattr(metric, 'error') and hasattr(metric, 'dEdy')), "Metric must implement error and error derivative."
+        self.error_metric = metric
         
     def error(self, target):
-        return self._output_layer.error(self._output_layer._activation, target)
+        return self.error_metric.error(self._output_layer._activation, target)
         
     def clear(self):
         """

cython_lstm/neuron.py
@@ -24,9 +24,6 @@ def softmax_unsafe(x):
     exped_distributions = np.exp(x.T)
     total_distribution = exped_distributions.sum(axis=0)
     return (exped_distributions / total_distribution).T
-    
-def softmax_error_one_hot(x, t):
-    return -np.log(x[np.arange(0,t.shape[0]), t]).sum()
 
 class Neuron():
     @staticmethod
@@ -40,18 +37,7 @@ class Neuron():
     
     @staticmethod
     def dydz(x):
-        return 1.0
-
-    @staticmethod
-    def dEdy(y, t):
-        return y - t
-
-    @staticmethod
-    def error(y, target):
-        """
-        Mean squared error : 1/2 (y-t)^2
-        """
-        return 0.5 * (y - target)**2
+        return np.float64(1.0).astype(x.dtype)
 
 class RectifierNeuron(Neuron):
     @staticmethod
@@ -76,14 +62,6 @@ class LogisticNeuron(Neuron):
         d/dx 1/ ( 1 + e^-x)  = d/dx sig(x) = sig(x) - sig(x)^2
         """
         return x - x**2
-
-    @staticmethod
-    def error(y, target):
-        """
-        Binary Cross entropy error:
-        D_{KL}(p || q) = sum_i { (1-p_i) * log(1 - q_i) -p_i log (q_i) }
-        """
-        return -(target * np.log(y) + (1.0 - target) * np.log1p(-y))
     
 class TanhNeuron(Neuron):
     @staticmethod
@@ -98,35 +76,8 @@ class TanhNeuron(Neuron):
         """
         return 1.0 - x**2
 
-    @staticmethod
-    def error(y, target):
-        """
-        Cross entropy error (we reshape tanh activation into
-        a sigmoid like activation and then apply cross entropy
-        criterion to it)
-        
-        """
-        resized_activation = (y + 1.0) / 2.0
-        return -(target * np.log(resized_activation) + (1.0 - target) * np.log1p(- resized_activation))
-    
 class SoftmaxNeuron(Neuron):
     @staticmethod
     def activation_function(x, out=None):
         """Softmax"""
         return softmax(x, out)
-
-    @staticmethod
-    def dEdy(y, t):
-        dEdy = y.copy()
-        dEdy[np.arange(0,t.shape[0]), t] -= 1.
-        return dEdy
-
-    @staticmethod
-    def error(y, target):
-        """
-        Cross entropy error (we reshape tanh activation into
-        a sigmoid like activation and then apply cross entropy
-        criterion to it) (One hot outputs)
-        
-        """
-        return softmax_error_one_hot(y, target)